751fd72bd96252cf386c5e87de174254b2b1addd
[TahaAlothman] <tahaaborawi@gmail.com>
2023-08-26 15:40:40 +0200

    add post details

--- posts/views.py
+++ posts/views.py
@@ -12,5 +12,7 @@ def post_list (request):
 
 
 
-def post_detail (request):
-    pass
+def post_detail (request,post_id):
+    data=Post.objects.get(id=post_id)
+    return render(request,'post_detail.html',{'post':data})
+    
